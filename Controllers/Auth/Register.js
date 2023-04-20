bb21a555428e45814b611b9d2a61df627bd7cb89
barhoumiwajdi <wajdi.barhoumi26@gmail.com>
2023-04-20 11:14:55 +0100

    Config

--- Controllers/Auth/Register.js
+++ Controllers/Auth/Register.js
@@ -1,16 +1,16 @@
-const Client = require('../../Models/Account')
+const User = require('../../Models/Account')
 const bcrypt = require('bcryptjs')
 
 exports.Register = async (req, res) => {
     try {
-        const found = await Client.findOne({ Email: req.body.Email })
+        const found = await User.findOne({ Email: req.body.Email })
         if (found) {
             res.status(400).send({ message: 'Email est déja enregistré.' })
         }
         else {
             const salt = bcrypt.genSaltSync(10);
             req.body.Password = bcrypt.hashSync(req.body.Password, salt);
-            await Client.create(req.body)
+            await User.create(req.body)
             res.send({ message: "Ajouté avec succés" })
         }
     }
