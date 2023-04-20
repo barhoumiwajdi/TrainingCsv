5486b51f64b5b7b1a005772dba6d15ad5840bb6c
barhoumiwajdi <wajdi.barhoumi26@gmail.com>
2023-04-20 11:14:47 +0100

    Models Config

--- Controllers/User/UserController.js
+++ Controllers/User/UserController.js
@@ -13,5 +13,15 @@ exports.getAllusers = async (req, res) => {
         res.status(500).send({ message: 'erruer serveur ' || error })
     }
 
+}
 
+exports.deleteUser = async (req, res) => {
+    try {
+
+        const deleted = await User.findOneAndDelete(req.params.iduser)
+        res.status(200).send({ message: 'user has been deleted ', deleted })
+
+    } catch (error) {
+        res.status(500).send({ message: 'erruer serveur ' || error })
+    }
 }
