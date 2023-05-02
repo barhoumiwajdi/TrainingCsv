4d4f6993044f10493cee97d576935ccbff4c05f5
barhoumiwajdi <wajdi.barhoumi26@gmail.com>
2023-05-02 16:59:24 +0100

    Reset Routes Config

--- Routes/Auth/AuthRoutes.js
+++ Routes/Auth/AuthRoutes.js
@@ -3,10 +3,13 @@ const express = require('express');
 const passport = require('passport');
 const { Login } = require('../../Controllers/Auth/Login');
 const { Register } = require('../../Controllers/Auth/Register');
+const { forgetPassword, resetPassword } = require('../../Controllers/Auth/ForgetandResetPassword');
 const router = express.Router();
 
 router.post('/Register', Register)
 router.post('/Login', Login)
+router.post('/forgetPassword', forgetPassword)
+router.put('/resetPassword/:token', resetPassword)
 router.get('/profile', passport.authenticate('bearer', { session: false }), (req, res) => {
     res.send(req.user)
 })
