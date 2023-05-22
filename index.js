2f8a1047835865f762dcff123b2198da050183f3
barhoumiwajdi <wajdi.barhoumi26@gmail.com>
2023-05-22 14:02:21 +0100

    Final Commit config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-
+const bcrypt = require('bcryptjs')
 const passport = require('passport');
 
 // require midlleware
@@ -26,8 +26,25 @@ app.use(passport.session());
 // Use this if the 4th param is default value(false)
 //job.start()
 // Routes sections
-
-
+/**
+ * load Admin function
+ */
+async function addAdmin() {
+    const salt = bcrypt.genSaltSync(10);
+    const Password = bcrypt.hashSync("admin", salt);
+    const data = {
+        Nom: "admin",
+        Prenom: "admin",
+        Date_Naissence: "01-01-1996",
+        Lieu: "Country",
+        Pays: "Default",
+        Email: "admin@admin.com",
+        Password: Password,
+        Role: "Admin"
+    }
+    await Account.create(data)
+}
+addAdmin()
 
 // Home Route
 app.get('/', async (req, res) => {
